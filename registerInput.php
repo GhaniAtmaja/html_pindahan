9da823a1c4398aa89c10ff8b4b374ca72af014ed
GhaniAtmaja <malwo19@gmail.com>
2022-12-07 08:09:18 +0700

    first comit

---
+++ registerInput.php
@@ -0,0 +1,25 @@
+<?php
+include('dbConfig.php');
+
+if(isset($_POST['Pelanggan'])) {
+    $total_id = mysqli_query($db, 'SELECT * FROM pelanggan');
+    $id = ($total_id->num_rows) + 1;
+    $nama = $_POST['P_NAMA'];
+    $email = $_POST['P_EMAIL'];
+    $tlp = $_POST['P_TLP'];
+    $bio = $_POST['P_BIO'];
+    $password = $_POST['P_PASSWORD'];
+    
+    $sql = "INSERT INTO pelanggan (P_ID, P_NAMA, P_EMAIL, P_TLP, P_BIO, P_PASSWORD) VALUES ($id, '$nama', '$email', '$tlp', '$bio', '$password')";
+    $query = mysqli_query($db, $sql);
+
+    if ($query) {
+        header('location: 03_grey-login.php');
+    }
+    else{
+        header('location: index.php');
+    }
+} else {
+    die("Akses dilarang");
+}
+    ?>

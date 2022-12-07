7afa72ad951f78d64b7789ae789c736fc4654649
HaidarFico <haidarficoi@gmail.com>
2022-12-07 09:15:58 +0700

    Added constraints checker

--- registerInput.php
+++ registerInput.php
@@ -1,6 +1,6 @@
 <?php
 include('dbConfig.php');
-
+include('constraintsChecker.php');
 if(isset($_POST['Pelanggan'])) {
     $total_id = mysqli_query($db, 'SELECT * FROM pelanggan');
     $id = ($total_id->num_rows) + 1;
@@ -9,7 +9,9 @@ if(isset($_POST['Pelanggan'])) {
     $tlp = $_POST['P_TLP'];
     $bio = $_POST['P_BIO'];
     $password = $_POST['P_PASSWORD'];
-    
+    if(!isPasswordValid($password) && !isNameValid($nama) && !isEmailValid($email) && !isTelephoneValid($tlp)){
+        header('location: 03_grey-login.php');     
+    }
     $sql = "INSERT INTO pelanggan (P_ID, P_NAMA, P_EMAIL, P_TLP, P_BIO, P_PASSWORD) VALUES ($id, '$nama', '$email', '$tlp', '$bio', '$password')";
     $query = mysqli_query($db, $sql);
 
